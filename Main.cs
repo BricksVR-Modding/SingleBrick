94a4a47e01d7c77a675d2681fdf3ecf8822f8f17
ATXLtheAxolotl <flashtube107@gmail.com>
2023-03-11 08:19:22 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using System.Collections;
+
+namespace BricksVR
+{
+    public static class BuildInfo
+    {
+        public const string Name = "SingleBrick";
+        public const string Description = "A single-player BricksVR Mod!";
+        public const string Author = "ATXLtheAxolotl#2134";
+        public const string Company = "BricksVR Modding";
+        public const string Version = "0.1.0";
+        public const string DownloadLink = "https://github.com/BricksVR-Modding/SingleBrick/releases";
+    }
+
+    public class SingleBrick : MelonMod
+    {
+        
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(BricksVR.BuildInfo.Description)]
+[assembly: AssemblyDescription(BricksVR.BuildInfo.Description)]
+[assembly: AssemblyCompany(BricksVR.BuildInfo.Company)]
+[assembly: AssemblyProduct(BricksVR.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BricksVR.BuildInfo.Author)]
+[assembly: AssemblyTrademark(BricksVR.BuildInfo.Company)]
+[assembly: AssemblyVersion(BricksVR.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BricksVR.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(BricksVR.SingleBrick), BricksVR.BuildInfo.Name, BricksVR.BuildInfo.Version, BricksVR.BuildInfo.Author, BricksVR.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("BricksVR", "BricksVR")]
